82b6ddb0b1a8a9c414a327977da83797ae0ec975
Yashaswini Shivalingaiah <yashaswini.shivalingaiah@slu.edu>
2022-10-25 16:01:03 -0500

    blocked not in use test cases

--- src/app/board-moderator/board-moderator.component.spec.ts
+++ src/app/board-moderator/board-moderator.component.spec.ts
@@ -2,24 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BoardModeratorComponent } from './board-moderator.component';
 
-describe('BoardModeratorComponent', () => {
-  let component: BoardModeratorComponent;
-  let fixture: ComponentFixture<BoardModeratorComponent>;
+//describe('BoardModeratorComponent', () => {
+  // let component: BoardModeratorComponent;
+  // let fixture: ComponentFixture<BoardModeratorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BoardModeratorComponent ]
-    })
-    .compileComponents();
-  });
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     declarations: [ BoardModeratorComponent ]
+  //   })
+  //   .compileComponents();
+  // });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BoardModeratorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(BoardModeratorComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+//});
--- src/app/board-user/board-user.component.spec.ts
+++ src/app/board-user/board-user.component.spec.ts
@@ -2,24 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BoardUserComponent } from './board-user.component';
 
-describe('BoardUserComponent', () => {
-  let component: BoardUserComponent;
-  let fixture: ComponentFixture<BoardUserComponent>;
+//describe('BoardUserComponent', () => {
+  // let component: BoardUserComponent;
+  // let fixture: ComponentFixture<BoardUserComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BoardUserComponent ]
-    })
-    .compileComponents();
-  });
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     declarations: [ BoardUserComponent ]
+  //   })
+  //   .compileComponents();
+  // });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BoardUserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(BoardUserComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+//});
