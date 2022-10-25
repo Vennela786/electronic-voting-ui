c9e78d672ae33b4b7207841dc4f497e573c04435
Balaji Goniguntla <balajinaidu@Balajis-Air.lan>
2022-10-24 20:36:49 -0500

    added login api integration

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { TokenStorageService } from '../_services/token-storage.service';
 import { Router } from '@angular/router'
+import { AppGlobals } from '../global/global-config'
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +20,8 @@ export class LoginComponent implements OnInit {
   errorMessage = '';
   roles: string[] = [];
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router, 
+    public appGlobals: AppGlobals, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
@@ -28,26 +31,39 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const { username, password } = this.form;
-
-    this.authService.login(username, password).subscribe(
-      data => {
-        this.tokenStorage.saveToken(data.accessToken);
-        this.tokenStorage.saveUser(data);
-
-        this.isLoginFailed = false;
-        this.isLoggedIn = true;
-        this.roles = this.tokenStorage.getUser().roles;
-        this.reloadPage();
+    console.log('in onsubmit')
+    let loginData = this.formBuilder();
+    this.authService.login(loginData.value).subscribe({
+      next: (res) => {
+        console.log('next-------',res);
+        if(res) {
+          this.appGlobals.loginUserDetail = res;
+          this.isLoggedIn = true;
+          this.router.navigate(['/menu']);
+        } else {
+          this.isLoginFailed = true;
+        }
       },
-      err => {
-        this.errorMessage = err.error.message;
-        this.isLoginFailed = true;
+      error: (err) => {
+        console.log("err-----", err)
+        if(err.error.message) {
+          this.errorMessage = err.error.message
+        } else {
+          this.errorMessage = err.error.errorDefinition.message;
+        }
+      },
+      complete: () =>{
+
       }
-    );
+    })
+  }
 
-    this.router.navigate(['/menu']);
-
+  formBuilder() {
+    const { username, password } = this.form;
+    return (this.fb.group({
+        userName: username,            
+        password: password
+    }));
   }
 
   reloadPage(): void {
