dc1c1289c6dd2a55a6bcd7860e83f5731b37e012
Balaji Goniguntla <balajinaidu@Balajis-Air.lan>
2022-11-11 11:24:59 -0600

    added api integration

--- src/app/edit-poll/edit-poll.component.ts
+++ src/app/edit-poll/edit-poll.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { PollService } from '../_services/poll.service';
 
 @Component({
@@ -18,16 +18,42 @@ export class EditPollComponent implements OnInit {
     endDate: new FormControl(''),
     pollId: new FormControl('')
   })
-  constructor(private fb: FormBuilder, private router: Router, private pollService: PollService, private activatedRoute: ActivatedRoute) { }
+  constructor(private fb: FormBuilder, private router: Router, private pollService: PollService, private route: ActivatedRoute) { }
 
   errorMessage = '';
   isPollUpdateFailed = false;
 
   getPoll() {
-    
+    console.log("pollID---", this.pollId)
+    this.pollService.get(this.pollId).subscribe({
+      next: (res) => {
+        console.log('next-------',res);
+        if(res) {
+          this.setBalletForm(res);
+        }
+      },
+      error: (err) => {
+        console.log("err-----", err)
+        this.isPollUpdateFailed = true;
+        if(err.error.message) {
+          this.errorMessage = err.error.message
+        } else {
+          this.errorMessage = err.error.errorDefinition.message;
+        }
+      },
+      complete: () =>{}
+    })
   }
 
-  
+  setBalletForm(res: any) {
+    this.updateBalletform = new FormGroup({
+      title: new FormControl(res.title),
+      description: new FormControl(res.description),
+      startDate: new FormControl(res.startDate),
+      endDate: new FormControl(res.endDate),
+      pollId: new FormControl(res.pollId)
+    })
+  }
 
   onClear(e: any) {
     if (e) e.preventDefault()
@@ -35,15 +61,31 @@ export class EditPollComponent implements OnInit {
   }
 
   onSubmit() {
-    
+    this.pollService.update(this.updateBalletform.value, this.pollId).subscribe({
+      next: (res) => {
+        console.log('next-------',res);
+        if(res) {
+          this.router.navigate(['/menu']);
+        }
+      },
+      error: (err) => {
+        console.log("err-----", err)
+        this.isPollUpdateFailed = true;
+        if(err.error.message) {
+          this.errorMessage = err.error.message
+        } else {
+          this.errorMessage = err.error.errorDefinition.message;
+        }
+      },
+      complete: () =>{
+        this.updateBalletform.reset()
+      }
+    })
   }
     
   ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(params => {
-      if (params['pollId'] !== undefined) {
-          this.pollId = +params['pollId'];
-      }
-  });
+    this.route.params.subscribe((params: Params) => this.pollId = params['pollId']);
+    this.getPoll()
   }
   
 }
