843d3b04518d486c7b4f60cce7a26f87342ad329
Vennela786 <112149392+Vennela786@users.noreply.github.com>
2022-11-29 12:53:44 -0600

    Adding routing to modifyQuestions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,8 @@ import { CreateBallotComponent } from './create-ballot/create-ballot.component'
 import { ViewPollsComponent } from './veiw-poll/view-poll.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { EditPollComponent } from './edit-poll/edit-poll.component';
+import { BallotQuestionsComponent } from './ballot-questions/ballot-questions.component';
+import { ModifyQuestionsComponent } from './modify-questions/modify-questions.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -26,6 +28,8 @@ const routes: Routes = [
   { path: 'createBallot', component: CreateBallotComponent },
   { path: 'view-poll', component: ViewPollsComponent },
   { path: 'edit-poll/:pollId', component: EditPollComponent},
+  { path : 'list-questions/:pollId', component: BallotQuestionsComponent},
+  { path : 'modify-questions/:pollId', component: ModifyQuestionsComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
